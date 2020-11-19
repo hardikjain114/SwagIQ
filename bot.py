0f3af6b105bc3a76971a18cf5be4d44d26cc98e5
hardikjain114 <74732648+hardikjain114@users.noreply.github.com>
2020-11-19 23:27:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,41 +9,36 @@ import multiprocessing
 import threading
 import concurrent
 
-BOT_OWNER_ROLE = 'fetch' # change to what you need
-#BOT_OWNER_ROLE_ID = "487951701827911681"
+BOT_OWNER_ROLE = 'runner' # change to what you need
+#BOT_OWNER_ROLE_ID = "778999965451288616"
   
- 
- 
+
 oot_channel_id_list =[
-    "694528602586480722", #LOCO Galaxy
-    "679260593005527041", #LOCO Trivia Eagle
-    "675593117734535208", #LOCO Dark Trivia
-    "681891490208677958", #LOCO The Best
-    "681092218479575177", #LOCO Marvel
-    "658225042231918603", #LOCO Trivia Nation
-    "569420128794443776", #LOCO United Nation
-    "694793854859083797", #LOCO Master Trivia
-    "675925058094497792", #FLIPKART GALAXY
-    "681891700377124906", #FLIPKART The Best
-    "690524518669615185", #SwagIq Infinity
-    "658225824482328586", #SwagIq Trivia Nation
-    "686419754449371158", #SwagIQ Galaxy
-    "690517716939046942", #Quipp QUIPP-bot
-    "691340679187791872", #QUIPP Royal Trivia
-    "679751812915789991", #QUIPP Dark Trivia
-    "694084730022264852", #QUIPP Trivia Eagle
-    "686419613898113092", #QUIPP Trivia Galaxy
-    "681904686256619570", #QUIPP The Best
-    "694957872081141811", #QUIPP Master Trivia
-    "690524571966898196" #QUIPP Trivia Infinity
+    "778780076354437161", #SwagBucks Sparks
+    "756445815001710632", #SwagBucks TRIVIA Bot
+    "729535056594337812", #SwagBucks Trivia Viper King
+    "774605363114803230", #SwagBucks Trivia Eagle
+    "743339722444636251", #Swagbucks Magic text
+    "755080891004354630", #SwagBucks Trivia Challenge
+    "747500994316992613", #SwagBucks Smart world
+    "747465526728720554", #SwagBucks Trivia Time
+    "773602513597235231", #SwagBucks DRAGON World
+    "776648141315112970", #SwagBucks Phoneix Trivia
+    "748859081737109595", #SWAGBUCKS BRIGHT TRIVIA
+    "773390146406055937", #SwagBucks Trivia Galaxy
+    "772056500217577515", #SwagBucks Google Pro
+    "740581485462945894", #SwagBucks Trivia Velocity
+    "774436021244788787", #SwagBucks Gaming Community
+    "773336365311590431", #SwagBucks Imperio
+    "774480600010457108", #SwagBucks UKT
 ]
 
 
 answer_pattern = re.compile(r'(not|n)?([1-3]{1})(\?)?(cnf|c)?(\?)?$', re.IGNORECASE)
 
-apgscore = 500
-nomarkscore = 350
-markscore = 250
+apgscore = 402
+nomarkscore = 348
+markscore = 252
 
 async def update_scores(content, answer_scores):
     global answer_pattern
@@ -86,7 +81,7 @@ class SelfBot(discord.Client):
 
     async def on_ready(self):
         print("======================")
-        print("Royal Trivia Self Bot")
+        print("Trivia Library Self Bot")
         print("Connected to discord.")
         print("User: " + self.user.name)
         print("ID: " + str(self.user.id))
@@ -143,16 +138,16 @@ class Bot(discord.Client):
         self.answer_scores = answer_scores
 
         # embed creation
-        self.embed=discord.Embed(title="**CONNECTED TO SERVER**", description="** FINDING BEST ANSWER **", color=0xADFF2F)
-        self.embed.set_author(name =' 😍 ©ROYAL TRIVIA OFFICIAL™ 🇮🇳',url=' ',icon_url='https://cdn.discordapp.com/attachments/691224840392278016/694145453570260992/IMG_20200312_174727.jpg')
-        self.embed.add_field(name="Option 1", value="0", inline=False)
-        self.embed.add_field(name="Option 2", value="0", inline=False)
-        self.embed.add_field(name="Option 3", value="0", inline=False)
-        self.embed.set_footer(text=f"𝕿𝖍𝖊 B𝖆𝖉𝖘𝖍𝖆𝖍 🇮🇳 #5776", \
-            icon_url="https://cdn.discordapp.com/avatars/487951701827911681/b49466d35068ecb9fae2b53754d33b6e.png?size=256")
+        self.embed=discord.Embed(title="**Connected With Crowd**", description="", color=0xADFF2F)
+        self.embed.set_author(name ='Trivia Company Pvt Ltd',url=' ',icon_url='https://cdn.discordapp.com/attachments/765487385885671425/765850682535706645/IMG_20201014_134652.png')
+        self.embed.add_field(name="**``Option 1``**", value="**0**", inline=False)
+        self.embed.add_field(name="**``Option 2``**", value="**0**", inline=False)
+        self.embed.add_field(name="**``Option 3``**", value="**0**", inline=False)
+        self.embed.set_footer(text=f"© Developed by The Badshah Company", \
+            icon_url="https://cdn.discordapp.com/attachments/765487385885671425/765850682535706645/IMG_20201014_134652.png")
         self.embed.set_thumbnail(url ='https://www.gstatic.com/allo/stickers/pack-8/v5/xxhdpi/7.gif')
-        # await message.add_reaction(emoji=' :white_check_mark: ')
-        # await self.bot.add_reaction(embed,' :white_check_mark: ', ':x:')
+        # await bot.add_reaction(message = "self.embed",emoji = ":white_check_mark:")
+        # await self.bot.add_reaction(embed,':rtlogo:')
 
 
 
@@ -192,12 +187,11 @@ class Bot(discord.Client):
 #             if answer == 3:
 #                 three_check = " :x:  "            
  
-        self.embed.set_field_at(0, name="**Option 1**", value="``{0}``{1}".format(lst_scores[0], one_check))
-        self.embed.set_field_at(1, name="**Option 2**", value="``{0}``{1}".format(lst_scores[1], two_check))
-        self.embed.set_field_at(2, name="**Option 3**", value="``{0}``{1}".format(lst_scores[2], three_check))
+        self.embed.set_field_at(0, name="**``Option 1``**", value="{0}{1}".format(lst_scores[0], one_check))
+        self.embed.set_field_at(1, name="**``Option 2``**", value="{0}{1}".format(lst_scores[1], two_check))
+        self.embed.set_field_at(2, name="**``Option 3``**", value="{0}{1}".format(lst_scores[2], three_check))
         if self.embed_msg is not None:
            await self.embed_msg.edit(embed=self.embed)
-           await msg.add_reaction(emoji=' :white_check_mark: ')
 
     async def on_ready(self):
         print("==============")
@@ -220,7 +214,7 @@ class Bot(discord.Client):
           #  id1 = pd[9:] 
           #  await message.channel.send(f'channel <#{id1}> selected.')
 
-        if message.content.lower() == ".":
+        if message.content.lower() == "+":
             await message.delete()
             if BOT_OWNER_ROLE in [role.name for role in message.author.roles]:
                 self.embed_msg = None
@@ -230,10 +224,10 @@ class Bot(discord.Client):
                     await message.channel.send('',embed=self.embed)
                 self.embed_channel_id = message.channel.id
             else:
-                await message.channel.send("**make ``fetch`` role and taken self** ")
+                await message.channel.send("**make ``runner`` role and taken self** ")
             return
 
-        if message.content.startswith('.help'):
+        if message.content.startswith('-help'):
           if BOT_OWNER_ROLE in [role.name for role in message.author.roles]:
            embed = discord.Embed(title="Help Commands", description="**How Run Bot**", color=0x00ff00)
            embed.add_field(name="Support Game", value="**Loco\nBrainbaazi\nPollbaazi\nSwag-iq\nThe-Q\nConfett-India\nCash-Quiz-Live\nHQ Tivia**", inline=False)
@@ -264,7 +258,7 @@ def bot_with_cyclic_update_process(update_event, answer_scores):
     upd_thread.start()
 
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start('NTk5NTU3NTMyNTgyNjc0NDUz.XoTtug.jHgfvn1gt1cFbiLnhoNQ2AvlnTs'))
+    loop.create_task(bot.start('Nzc4OTk5OTY1NDUxMjg4NjE2.X7aKQA.X5xGjFhRUDFtut5Ky8dW8GQVqbs'))
     loop.run_forever()
 
 
@@ -273,7 +267,7 @@ def selfbot_process(update_event, answer_scores):
     selfbot = SelfBot(update_event, answer_scores)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(selfbot.start('NDg3OTUxNzAxODI3OTExNjgx.Xf4IXA.4Lg60LIjHSNyVC1iNYvJNUW9NbA',
+    loop.create_task(selfbot.start('mfa.ij41le1k9-sWEFkSOgWnYpVgsHlArRiaCqcKW1w52nmodXRtUwbutPDtNp6rfMUKPNcHegQuzHVXOuIu0RCJ',
                                     bot=False))
     loop.run_forever()
 
